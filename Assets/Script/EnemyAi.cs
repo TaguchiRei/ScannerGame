4fcd71a27bf96255ba883e1b3e2a55fb5d8faae6
TaguchiRei <ren.len2426@gmail.com>
2025-03-06 16:10:05 +0900

    Enemyの移動を制御するスクリプト

--- Assets/Script/EnemyAi.cs
+++ Assets/Script/EnemyAi.cs
@@ -1,14 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EnemyAi : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private VisualEffect _visualEffect;
+    [SerializeField] private float _moveSpeed = 2;
+    [SerializeField] private Vector3 _velocity;
+    
+    private GameObject _playerObj;
+    private int _particleCount = 0;
+
+    private void Start()
     {
-        
+        _playerObj = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        
+        if (_playerObj != null)
+        {
+            if (Vector3.Distance(_playerObj.transform.position, transform.position) <= 50f)
+            {
+                transform.rotation = Quaternion.LookRotation(_playerObj.transform.position - transform.position);
+                _rigidbody.linearVelocity = transform.forward * _moveSpeed;
+            }
+        }
+        else
+        {
+            Debug.Log("Player object not found");
+        }
+    }
+
+    public void AddParticle()
+    {
+        _particleCount++;
+        _visualEffect.SetInt("Count",_particleCount);
+        _visualEffect.Play();
     }
 }
