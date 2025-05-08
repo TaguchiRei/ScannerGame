57694f3dd31163f2b97b46f40ec84fc9522b4e1e
TaguchiRei <ren.len2426@gmail.com>
2025-05-09 02:40:20 +0900

    [Fix]初めから作り直し

--- Assets/Script/GroundScript/GroundGenerator.cs
+++ Assets/Script/GroundScript/GroundGenerator.cs
@@ -10,6 +10,7 @@ public class GroundGenerator : MonoBehaviour
     private void Start()
     {
         CreateMaze(11);
+        /*
         StringBuilder st = new StringBuilder();
         for (int i = 0; i < 11; i++)
         {
@@ -23,88 +24,20 @@ public class GroundGenerator : MonoBehaviour
 
         string maze = st.ToString();
         Debug.Log(maze);
+        */
     }
 
 
     /// <summary>
-    /// 
+    /// 迷路生成を行う
     /// </summary>
     /// <param name="size">必ず奇数を入力</param>
     void CreateMaze(int size)
     {
-        Debug.Log("Generating");
         _maze = new bool[size, size];
-        Stack<(int x, int y)> road = new();
-
-        //初期地点をランダムな偶数の座標に決定する
-        var x = Random.Range(1, size / 2 + 1);
-        var y = Random.Range(1, size / 2 + 1);
-
-        road.Push((x * 2, y * 2));
-        _maze[x, y] = true;
-        while (road.Count != 0)
-        {
-            List<(int, int)> checkedPoints = new();
-            var roadPos = road.Pop();
-
-            //２個先のセルを探索
-            (int, int)[] checkPos =
-            {
-                (roadPos.x, roadPos.y - 2),
-                (roadPos.x, roadPos.y + 2),
-                (roadPos.x - 2, roadPos.y),
-                (roadPos.x + 2, roadPos.y)
-            };
-            for (int i = 0; i < 4; i++)
-            {
-                if (AllCheck(checkPos[i].Item1, checkPos[i].Item2, size, i)) ;
-            }
-
-
-            if (checkedPoints.Count == 0)
-            {
-                Debug.Log(roadPos);
-                continue; //周囲がすべて探索済みならそのまま戻る
-            }
-
-            //未探索セルがあった場合最初にpopした値を戻してからそのセルを保存
-            var selectPos = checkedPoints[Random.Range(0, checkedPoints.Count)];
-            road.Push(roadPos);
-            road.Push(selectPos);
-            _maze[selectPos.Item1, selectPos.Item1] = true;
-        }
-
-        Debug.Log($"Generated {_maze.Length}");
+        Stack<(int, int)> road = new Stack<(int, int)>();
+        var startPos = (Random.Range(0, size/2) * 2, Random.Range(0, size/2) * 2);
+        Debug.Log(startPos.ToString());
     }
-
-    /// <summary>
-    /// 迷路の範囲内且つ未探索の場合trueを返す
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <param name="size"></param>
-    /// <param name="numberA"></param>
-    /// <returns></returns>
-    private bool AllCheck(int x, int y, int size, int numberA)
-    {
-        if (y <= 0 || y >= size || x <= 0 || x >= size || _maze[x, y]) return false;
-        _maze[x, y] = true;
-        switch (numberA)
-        {
-            case 0:
-                _maze[x, y + 1] = true;
-                break;
-            case 1:
-                _maze[x, y - 1] = true;
-                break;
-            case 2:
-                _maze[x + 1, y] = true;
-                break;
-            default:
-                _maze[x - 1, y] = true;
-                break;
-        }
-
-        return true;
-    }
+    
 }
