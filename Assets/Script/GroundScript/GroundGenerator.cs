518100aaeb93649c3633e7f5b6e6f4afdb2ee633
TaguchiRei <ren.len2426@gmail.com>
2025-05-09 01:48:10 +0900

    [Add]非再帰バックトラッキング法(穴掘り法)による迷路作成アルゴリズム作成

--- Assets/Script/GroundScript/GroundGenerator.cs
+++ Assets/Script/GroundScript/GroundGenerator.cs
@@ -8,7 +8,8 @@ public class GroundGenerator : MonoBehaviour
     
     private void Start()
     {
-        CreateMaze(5);
+        CreateMaze(11);
+        
     }
 
     
@@ -21,29 +22,51 @@ public class GroundGenerator : MonoBehaviour
     {
         _maze = new bool[size, size];
         Stack<(int x,int y)> road = new();
-        List<(int x,int y)> checkedPoints = new();
         
         //初期地点をランダムな偶数の座標に決定する
         var x = Random.Range(1, size / 2 + 1);
         var y = Random.Range(1, size / 2 + 1);
-        var startPos = new Vector2(x * 2, y * 2);
 
         road.Push((x * 2, y * 2));
         while (road.Count == 0)
         {
+            List<(int,int)> checkedPoints = new();
             var roadPos = road.Pop();
             
             //２個先のセルを探索
-            (int x,int y) up = (roadPos.x , roadPos.y - 2);
-            (int x,int y) down = (roadPos.x , roadPos.y + 2);
-            (int x,int y) left = (roadPos.x - 2, roadPos.y);
-            (int x,int y) right = (roadPos.x + 2, roadPos.y);
-
-            //セルが範囲外でないか、探索済みかどうかを判定
-            if (up.y > 0 && up.y < size)
+            (int, int)[] checkPos =
             {
-                
+                (roadPos.x , roadPos.y - 2),
+                (roadPos.x , roadPos.y + 2),
+                (roadPos.x - 2, roadPos.y),
+                (roadPos.x + 2, roadPos.y)
+            };
+            foreach (var pos in checkPos)
+            {
+                if(AllCheck(pos.Item1, pos.Item2, size)) checkedPoints.Add(pos);
             }
+            
+            if(checkedPoints.Count == 0) continue;//周囲がすべて探索済みならそのまま戻る
+            //未探索セルがあった場合保存する
+            road.Push(roadPos);
+            var selectPos = checkedPoints[Random.Range(0, checkedPoints.Count)];
+            road.Push(selectPos);
+            _maze[selectPos.Item1,selectPos.Item1] = true;
+            
         }
+    }
+
+    /// <summary>
+    /// 迷路の範囲内且つ未探索の場合trueを返す
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="size"></param>
+    /// <returns></returns>
+    private bool AllCheck(int x, int y, int size)
+    {
+        if (y <= 0 || y >= size || _maze[x, y] || x <= 0 || x >= size) return false;
+        _maze[x, y] = true;
+        return true;
     }
 }
