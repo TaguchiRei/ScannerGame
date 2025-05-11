a517ceb7c902fe3a074f2cdb783e24ee8ae07341
TaguchiRei <ren.len2426@gmail.com>
2025-05-11 18:03:54 +0900

    [Fix]探索地点の更新を行っていなかった問題を修正

--- Assets/Script/Maze/DepthFirstSearch.cs
+++ Assets/Script/Maze/DepthFirstSearch.cs
@@ -10,7 +10,7 @@ public class DepthFirstSearch : MazeBase
     [SerializeField] private MazeBase _mazeBase;
     [SerializeField] private MazePosition _startPos;
     [SerializeField] private MazePosition _endPos;
-    
+
     private bool[,] _visited;
 
     private void Start()
@@ -22,21 +22,22 @@ public class DepthFirstSearch : MazeBase
         {
             st.Append($" → {pos}");
         }
+
         st.Append("[goal]");
         Debug.Log(st.ToString());
     }
 
-    private List<(int,int)> SearchStart(int size)
+    private List<(int, int)> SearchStart(int size)
     {
         Maze = _mazeBase.Maze;
-        _visited = new bool[Maze.GetLength(0),Maze.GetLength(1)];
+        _visited = new bool[Maze.GetLength(0), Maze.GetLength(1)];
 
         if (!Maze[_endPos.x, _endPos.y])
         {
             Debug.Log("Goal is Wall");
-            return new List<(int,int)>();
+            return new List<(int, int)>();
         }
-        
+
         Stack<(int, int)> route = new();
         route.Push((_startPos.x, _startPos.y));
         _visited[_startPos.x, _startPos.y] = true;
@@ -45,20 +46,22 @@ public class DepthFirstSearch : MazeBase
         while (!goal)
         {
             var pos = route.Pop();
-            if(pos == (_endPos.x, _endPos.y)) goal = true;
-            var nextRouteOption = GetUncheckCellNext(pos, size);
-            if(nextRouteOption.Count == 0) continue;//道が行きどまりまたはすべて探索済みなら戻る
-            var nextRoute = nextRouteOption[0];
+            if (pos == (_endPos.x, _endPos.y)) goal = true;
+            var nextDirectionOption = GetUncheckCellNext(pos, size);
+            if (nextDirectionOption.Count == 0) continue; //道が行きどまりまたはすべて探索済みなら戻る
+
+            var nextRoute = (pos.Item1 + nextDirectionOption[0].Item1, pos.Item2 + nextDirectionOption[0].Item2);
             route.Push(pos);
             route.Push(nextRoute);
             _visited[nextRoute.Item1, nextRoute.Item2] = true;
             i++;
-            if (i > 50)
+            if (i > 200)
             {
                 Debug.Log("無限ループの可能性があります");
                 break;
             }
         }
+
         return route.ToList();
     }
 
@@ -68,7 +71,7 @@ public class DepthFirstSearch : MazeBase
         public int x;
         public int y;
     }
-    
+
     /// <summary>
     /// 隣接する未調査地点を取得する。
     /// </summary>
@@ -84,17 +87,14 @@ public class DepthFirstSearch : MazeBase
             if (checkPos.Item1 <= 0
                 || checkPos.Item1 >= size
                 || checkPos.Item2 <= 0
-                || checkPos.Item2 >= size)
-            {
-                Debug.Log($"Route Not Found At : {point}");
-                continue;
-            }
-            
+                || checkPos.Item2 >= size) continue;
+
             if (Maze[checkPos.Item1, checkPos.Item2] && !_visited[checkPos.Item1, checkPos.Item2])
             {
                 unexplored.Add(point);
             }
         }
+
         return unexplored;
     }
 }
